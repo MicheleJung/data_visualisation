a50c6145a16df313c9fc06069493661f0501a504
MicheleJung <141242173+MicheleJung@users.noreply.github.com>
2023-08-03 10:53:49 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -18,8 +18,8 @@ x_axis = st.selectbox('select element', el_list)
 
 st.multiselect('select location', file_name_list, file_name_list [0])
 
-x = df[Mg]
-y = df[Si]
+x = [1, 2, 3, 4, 5]
+y = [6, 7, 2, 4, 5]
 
 p = figure(
     title='simple scatter example',
