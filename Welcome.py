f3e036d6465b8eb85caa9820e49b3ea228430c6b
MicheleJung <141242173+MicheleJung@users.noreply.github.com>
2023-08-03 12:03:35 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -18,8 +18,8 @@ x_axis = st.selectbox('select element', el_list)
 
 st.multiselect('select location', file_name_list, file_name_list [0])
 
-x = ['el_list'] # das muss irgendwie geändert werden
-y = ['el_list']
+x = df['el_list'] # das muss irgendwie geändert werden
+y = df['el_list']
 
 p = figure(
     title='simple scatter example',
